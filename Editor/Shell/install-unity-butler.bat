8c55cb01964b5562e5da41300f59202aa2f240b0
Mark Mayer <mdm373@gmail.com>
2021-04-21 01:07:20 -0400

    First Pass

---
+++ Editor/Shell/install-unity-butler.bat
@@ -0,0 +1,15 @@
+﻿ECHO OFF
+
+echo "Downloading Latest Butler"
+curl -L -o butler.zip https://broth.itch.ovh/butler/windows-amd64/LATEST/archive/default
+
+echo "Extracting"
+if not exist  "%UNITY_APP_PATH%/RDG/UnityButler/bin" mkdir "%UNITY_APP_PATH%/RDG/UnityButler/bin"
+tar -x -C %UNITY_APP_PATH%/RDG/UnityButler/bin -f butler.zip
+del butler.zip
+
+echo "Ignoring From Repo"
+echo * 1> %UNITY_APP_PATH%/RDG/UnityButler/.gitignore
+
+PAUSE
+
